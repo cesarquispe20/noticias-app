61a5489f7764c50873b2e93371f746d06e9de4f9
cesarquispe <cesarquispe.20@gmail.com>
2021-08-12 23:35:47 -0500

    se agregó la entidad noticia

---
+++ src/main/java/ms/com/quispe/app/noticias/entity/Noticia.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package ms.com.quispe.app.noticias.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+/**
+ * @author Pedro Quispe
+ *
+ */
+@Entity
+@Data
+@Table(name = "noticas")
+public class Noticia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 200, nullable = false)
+	private String titulo;
+	
+	private String detalle;
+	
+	@Column(name = "fecha_creacion")
+	private Date fechaCreacion;	
+	
+	private String estatus;
+}
