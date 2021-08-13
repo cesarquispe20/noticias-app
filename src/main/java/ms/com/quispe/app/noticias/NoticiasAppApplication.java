f67befd9fd83166fc0a2c5b52fa922393ea92f32
cesarquispe <cesarquispe.20@gmail.com>
2021-08-12 22:15:26 -0500

    incio del proyecto noticias

---
+++ src/main/java/ms/com/quispe/app/noticias/NoticiasAppApplication.java
@@ -0,0 +1,13 @@
+package ms.com.quispe.app.noticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ms/com/quispe/app/noticias/NoticiasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.com.quispe.app.noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
